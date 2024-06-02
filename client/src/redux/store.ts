ef1edd48d0ab42489bb97e864029728b1709eda4
marwanEsm <marwan.esmaail85@gmail.com>
2024-06-02 10:50:06 +0200

    fix bugs

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
-import { thunk } from "redux-thunk";
+import thunk from "redux-thunk";
 
-
+// Configure the store
 export const store = configureStore({
-    reducer: { rootReducer },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk as any)
-})
+    reducer: rootReducer
+});
 
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppStore = typeof store;
 
-
-
-
+// Export the store
+export default store;
