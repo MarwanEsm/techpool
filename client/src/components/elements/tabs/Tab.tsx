10806e203e9b1d47a9a9fdede091950528272e7a
marwanEsm <marwan.esmaail85@gmail.com>
2024-06-09 12:48:10 +0200

    modify tab style

--- client/src/components/elements/tabs/Tab.tsx
+++ client/src/components/elements/tabs/Tab.tsx
@@ -1,29 +1,29 @@
-import { Nav, NavItem, NavLink } from "reactstrap"
-import styles from "./Tab.module.scss"
+import styles from "./Tab.module.scss";
 
 interface ITabItem {
-    label: string,
-    index: number,
+    label: string;
+    index: number;
 }
 
 interface ITabItemsProps {
-    items: ITabItem[],
-    selectedTab: number,
-    onClick: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, index: number) => void
+    items: ITabItem[];
+    selectedTab: number;
+    onClick: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, index: number) => void;
 }
-
 
 const Tab = (props: ITabItemsProps) => {
-
-    return <Nav tabs className={styles.tab}  >
-        {props.items.map((item, index) =>
-            <NavItem key={index}>
-                <NavLink active={props.selectedTab === index} onClick={(e) => props.onClick(e, index)}>
-                    {item.label}
-                </NavLink>
-            </NavItem>
-        )}
-    </Nav>
+    return (
+        <ul className={styles.tab}>
+            {props.items.map((item, index) => (
+                <li key={index}>
+                    <a className={props.selectedTab === index ? 'active' : ''}
+                        onClick={(e) => props.onClick(e, index)}>
+                        {item.label}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    );
 }
 
-export default Tab
+export default Tab;
