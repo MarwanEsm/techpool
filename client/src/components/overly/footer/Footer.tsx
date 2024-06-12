97c71ad894202eae134c15915a38a3f6e27645c9
marwanEsm <marwan.esmaail85@gmail.com>
2024-06-12 18:37:01 +0200

    modify style

--- client/src/components/overly/footer/Footer.tsx
+++ client/src/components/overly/footer/Footer.tsx
@@ -14,15 +14,14 @@ function Footer() {
     return (
         <div>
             <footer className={styles.footer}>
-                <div>
-                    <p className={styles.links}>
-                        <Link href="/">Home</Link>
-                        <Link href="/blog">Blog</Link>
-                        <Link href="/jobs">Jobs</Link>
-                        <Link href="/contact">Contact</Link>
-                    </p>
-                    <p className="company-name">Marwan Trap © 2021</p>
-                </div>
+
+                <p className={styles.links}>
+                    <Link href="/">Home</Link>
+                    <Link href="/blog">Blog</Link>
+                    <Link href="/jobs">Jobs</Link>
+                    <Link href="/contact">Contact</Link>
+                </p>
+
                 <div>
                     <div>
                         {/* //TODO:add google map card <a
@@ -35,7 +34,7 @@ function Footer() {
 
                     </div>
 
-                    <div>
+                    {/* <div>
                         <FontAwesomeIcon icon={faPhone} />
                         <p>+49 15755076336</p>
                     </div>
@@ -43,25 +42,22 @@ function Footer() {
                     <div>
                         <FontAwesomeIcon icon={faEnvelope} />
                         <p onClick={handelClick}>support@bbbootstrap.com</p>
-                    </div>
+                    </div> */}
                 </div>
 
-                <div>
-
-                    <div className={styles.icon}>
-                        <Link href="https://www.facebook.com/profile.php?id=100014692329207">
-                            <FontAwesomeIcon icon={faFacebook} />
-                        </Link>
-                        <Link href="https://twitter.com/MarwanAbouEsmaa">
-                            <FontAwesomeIcon icon={faTwitter} />
-                        </Link>
-                        <Link href="https://www.linkedin.com/in/marwan-esmaail/">
-                            <FontAwesomeIcon icon={faLinkedin} />
-                        </Link>
-                        <Link href="https://github.com/MarwanEsm">
-                            <FontAwesomeIcon icon={faGithub} />
-                        </Link>
-                    </div>
+                <div className={styles.icon}>
+                    <Link href="https://www.facebook.com/profile.php?id=100014692329207">
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </Link>
+                    <Link href="https://twitter.com/MarwanAbouEsmaa">
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/marwan-esmaail/">
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </Link>
+                    <Link href="https://github.com/MarwanEsm">
+                        <FontAwesomeIcon icon={faGithub} />
+                    </Link>
                 </div>
 
             </footer>
